45b547544a06e8709f464d394a4b697a87876cc9
Artaslim <Taslimuddinar@gmail.com>
2022-10-27 01:07:47 +0600

    route-create

---
+++ src/Pages/Blog/Blog/Blog.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Blog = () => {
+  return (
+    <div>
+      <h2>thes is bog</h2>
+    </div>
+  );
+};
+
+export default Blog;
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,10 +1,31 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../layout/Main";
+import Blog from "../../Pages/Blog/Blog/Blog";
+import Courses from "../../Pages/Courses/Courses/Courses";
+import Faq from "../../Pages/FAQ/FAQ/Faq";
+import Item from "../../Pages/Shared/Item/Item";
 
 export const routes = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
-    children: [],
+    children: [
+      {
+        path: "/item",
+        element: <Item></Item>,
+      },
+      {
+        path: "/courses",
+        element: <Courses></Courses>,
+      },
+      {
+        path: "/faq",
+        element: <Faq></Faq>,
+      },
+      {
+        path: "/blog",
+        element: <Blog></Blog>,
+      },
+    ],
   },
 ]);
