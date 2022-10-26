df2d68162ee2ab03cb5a6681928e6b9e0638c47e
Artaslim <Taslimuddinar@gmail.com>
2022-10-26 23:19:51 +0600

    install done

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCyO5Dok1dQgM2FI59ycogjqYyvbU4x94A",
+  authDomain: "online-courses-57ec6.firebaseapp.com",
+  projectId: "online-courses-57ec6",
+  storageBucket: "online-courses-57ec6.appspot.com",
+  messagingSenderId: "490501750009",
+  appId: "1:490501750009:web:38b1e38692acb79e53b628",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
